45ccc8ca5a0bb33b5f1a789b184287cbdabee737
SaiKumar Immadi <saikumar@SaiKumars-MacBook-Pro.local>
2020-06-30 01:19:37 +0530

    swipe gestures added

--- screens/NowPlaying/index.js
+++ screens/NowPlaying/index.js
@@ -1,28 +1,36 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {SafeAreaView, StyleSheet, FlatList, View, Text} from 'react-native';
+import GestureRecognizer from 'react-native-swipe-gestures';
 
 import MovieTile from '../../components/MovieTile';
 import ActivityIndicatorView from '../../components/ActivityIndicatorView';
+import {routes} from '../../components/AppNavigator/routes';
 
 class NowPlaying extends Component {
+  onSwipeLeft = () => {
+    this.props.navigation.navigate(routes.favorites.routeName);
+  };
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <ActivityIndicatorView loading={this.props.moviesLoading}>
-          {this.props.nowPlaying.length === 0 ? (
-            <View style={styles.nothingTextContainer}>
-              <Text style={styles.nothingText}>Nothing to show here.</Text>
-            </View>
-          ) : (
-            <FlatList
-              data={this.props.nowPlaying}
-              keyExtractor={item => `${item.id}`}
-              renderItem={({item: movie}) => (
-                <MovieTile key={movie.id} movie={movie} />
-              )}
-            />
-          )}
+          <GestureRecognizer style={{flex: 1}} onSwipeLeft={this.onSwipeLeft}>
+            {this.props.nowPlaying.length === 0 ? (
+              <View style={styles.nothingTextContainer}>
+                <Text style={styles.nothingText}>Nothing to show here.</Text>
+              </View>
+            ) : (
+              <FlatList
+                data={this.props.nowPlaying}
+                keyExtractor={item => `${item.id}`}
+                renderItem={({item: movie}) => (
+                  <MovieTile key={movie.id} movie={movie} />
+                )}
+              />
+            )}
+          </GestureRecognizer>
         </ActivityIndicatorView>
       </SafeAreaView>
     );
