9bf07b3cdd77c5642be567c9264cc331b490998c
SaiKumar Immadi <saikumar@SaiKumars-MacBook-Pro.local>
2020-07-03 02:50:51 +0530

    updated loader for card view

--- components/ActivityIndicatorView/index.js
+++ components/ActivityIndicatorView/index.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import {View, ActivityIndicator, StyleSheet} from 'react-native';
+import React, {Fragment} from 'react';
+import {View, StyleSheet} from 'react-native';
+import ContentLoader, {Rect} from 'react-content-loader/native';
 
 const ActivityIndicatorView = ({loading, children}) => (
   <View style={styles.container} pointerEvents={loading ? 'none' : 'auto'}>
     {loading && (
-      <View style={styles.activityIndicatorContainer}>
-        <ActivityIndicator size="large" />
-      </View>
+      <ContentLoader backgroundColor="#CCC" style={{flex: 1}}>
+        {Array(5)
+          .fill(1)
+          .map((x, index) => (
+            <Fragment key={index}>
+              <Rect
+                x="5%"
+                y={`${15 + index * 215}`}
+                rx="5"
+                ry="5"
+                width="30%"
+                height="200"
+              />
+              <Rect
+                x="40%"
+                y={`${15 + index * 215}`}
+                rx="5"
+                ry="5"
+                width="50%"
+                height="20"
+              />
+              <Rect
+                x="40%"
+                y={`${50 + index * 215}`}
+                rx="5"
+                ry="5"
+                width="42%"
+                height="20"
+              />
+              <Rect
+                x="40%"
+                y={`${85 + index * 215}`}
+                rx="5"
+                ry="5"
+                width="35%"
+                height="20"
+              />
+            </Fragment>
+          ))}
+      </ContentLoader>
     )}
-    {children}
+    {!loading && children}
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  activityIndicatorContainer: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.05)',
   },
 });
 
